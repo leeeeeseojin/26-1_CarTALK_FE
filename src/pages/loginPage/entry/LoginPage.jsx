61058a0e0773edd841467d0f45da8ca8eaf1dcd1
leeeeeseojin <kkyuuu00@gmail.com>
2026-04-04 02:53:53 +0900

    design: add LoginPage UI

--- src/pages/loginPage/entry/LoginPage.jsx
+++ src/pages/loginPage/entry/LoginPage.jsx
@@ -1,9 +1,53 @@
-function LoginPage() {
+import InputField from '../../../components/inputField/InputField'
+import './LoginPage.css'
+
+export default function LoginPage() {
   return (
-    <div>
-      <h1>로그인</h1>
-    </div>
+    <main className='login'>
+      <div className='login__wrap'>
+        {/* 로고 */}
+        <h1 className='login__brand'>CarTALK</h1>
+
+        {/* 로그인 카드 */}
+        <section className='login__card' aria-label='로그인'>
+          <div className='login__fields'>
+            {/* 이메일 입력 */}
+            <InputField
+              id='email'
+              type='email'
+              name='email'
+              placeholder='이메일을 입력하세요'
+              autoComplete='email'
+              required
+            />
+
+            {/* 비밀번호 입력 */}
+            <InputField
+              id='password'
+              type='password'
+              name='password'
+              placeholder='비밀번호를 입력하세요'
+              autoComplete='current-password'
+              required
+            />
+
+            {/* 로그인 버튼 */}
+            <button className='login__btn--primary' type='submit'>
+              로그인
+            </button>
+
+            {/* 회원가입 버튼 */}
+            <button className='login__btn--ghost' type='button'>
+              회원가입
+            </button>
+          </div>
+
+          {/* 비밀번호 찾기 링크 */}
+          <a className='login__forgot' href='#'>
+            비밀번호를 잊으셨나요?
+          </a>
+        </section>
+      </div>
+    </main>
   )
 }
-
-export default LoginPage
